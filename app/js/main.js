d19034b285461b985f573d9d58a7b8a0bed2127b
pravinneosoft <pravin.satpute@neosoftmail.com>
2021-12-13 10:03:29 +0530

    pages update

--- app/js/main.js
+++ app/js/main.js
@@ -31,21 +31,53 @@
   })(jQuery); 
 
 
-  let options = {
-    startAngle: -1.55,
-    size: 127,
-    value: 0.85,
-    fill: {
-      gradient: ['red', 'red']
-    }
-  }
-  $(".progres__circle .progres__bar").circleProgress(options).on('circle-animation-progress',
-    function (event, progress, stepValue) {
-      $(this).parent().find(".progres__percent").text(String(stepValue.toFixed(2).substr(2)) + "%");
-    });
-  $(".js .progres__bar").circleProgress({
-    value: 0.70   
-  });
-  $(".react .progres__bar").circleProgress({
-    value: 0.60
-  });
+  // let options = {
+  //   startAngle: -1.55,
+  //   size: 127,
+  //   value: 0.85,
+  //   fill: {
+  //     gradient: ['black', 'blue']
+  //   }
+  // }
+  // $(".progres__circle .progres__bar").circleProgress(options).on('circle-animation-progress',
+  //   function (event, progress, stepValue) {
+  //     $(this).parent().find(".progres__percent").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  //   });
+  // $(".js .progres__bar").circleProgress({
+  //   value: 0.70   
+  // });
+  // $(".react .progres__bar").circleProgress({
+  //   value: 0.60
+  // });
+
+  $('.testimonial__row').slick({
+    dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+});
