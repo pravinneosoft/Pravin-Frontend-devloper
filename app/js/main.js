fb82e38d12a916ce7a86f7197d5502a4c75829b5
pravinneosoft <pravin.satpute@neosoftmail.com>
2021-12-15 09:50:52 +0530

    pages updat

--- app/js/main.js
+++ app/js/main.js
@@ -31,24 +31,66 @@
   })(jQuery); 
 
 
-  // let options = {
-  //   startAngle: -1.55,
-  //   size: 127,
-  //   value: 0.85,
-  //   fill: {
-  //     gradient: ['black', 'blue']
-  //   }
-  // }
-  // $(".progres__circle .progres__bar").circleProgress(options).on('circle-animation-progress',
-  //   function (event, progress, stepValue) {
-  //     $(this).parent().find(".progres__percent").text(String(stepValue.toFixed(2).substr(2)) + "%");
-  //   });
-  // $(".js .progres__bar").circleProgress({
-  //   value: 0.70   
-  // });
-  // $(".react .progres__bar").circleProgress({
-  //   value: 0.60
-  // });
+  let options = {
+    startAngle: -1.55,
+    size: 127,
+    value: 0.85,
+    fill: {
+      gradient: ['#841e25', '#841e25']
+    }
+  }
+  $(".progres__circle .progres__bar").circleProgress(options).on('circle-animation-progress',
+    function (event, progress, stepValue) {
+      $(this).parent().find(".progres__percent").text(String(stepValue.toFixed(2).substr(2)) + "%");
+    });
+  $(".js .progres__bar").circleProgress({
+    value: 0.70,
+    fill: {
+      gradient: ['#70ce88', '#70ce88']
+    }
+  });
+  $(".react .progres__bar").circleProgress({
+    value: 0.60,
+    fill: {
+      gradient: ['#ffba49', '#ffba49']
+    }
+  });
+
+
+  $('.education__row').slick({
+    
+  infinite: true,
+  
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow:'<div class="prev"><span class="icon-arrow-left"></span></div>',
+  nextArrow:'<div class="next"><span class="icon-arrow-right"></span></div>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+});
+
 
   $('.testimonial__row').slick({
     dots: true,
@@ -67,14 +109,14 @@
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 767,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 576,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
