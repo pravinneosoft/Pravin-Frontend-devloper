af62f879886791dd022243cb046988da0c635a3f
pravinneosoft <pravin.satpute@neosoftmail.com>
2022-02-14 11:00:08 +0530

    demo-update

--- tailwind-demo/tailwind.config.js
+++ tailwind-demo/tailwind.config.js
@@ -21,6 +21,8 @@ module.exports = {
       'silver': '#ecebff',
       'amber-400': '#ffb016',
       'bermuda': '#78dcca',
+      'orange': '#f57629',
+      'black-rgba': 'rgba(0, 0, 0, 0.54)',
     },
     fontSize: {
       'xs': '15px',
@@ -35,13 +37,18 @@ module.exports = {
       '5xl': '3rem',
       '6xl': '4rem',
       '7xl': '70px',
+      '8xl': '44px',
+      '9xl': '133px',
+      '1xl': '2.5rem',
     },
     boxShadow: {
       '2xl': '-10px 10px #ffb016',
     },
     extend: {
       width: {
+        '256':'611px',
         '128': '276px',
+        '125': '254px',
         '76':'76%',
         '11':'11.4%',
         '2/9':'30%',
@@ -50,6 +57,10 @@ module.exports = {
         '12': '3rem',
         '14':'3.5rem',
         '16':'4.5rem',
+        '18':'5rem',
+      },
+      spacing: {
+        '128': '275px',
       }
     },
   },
