d95442905797fe7dd2375060711cd0c16cab07ae
pravinneosoft <pravin.satpute@neosoftmail.com>
2022-03-03 10:20:07 +0530

    medpenny update

---
+++ bootstrap5 demo/app/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("#link").click(function (e) {
+      e.preventDefault();
+      window.location.href = "files/Sample.pdf";
+  });
+});
+$(document).ready(function () {
+  $("#link").click(function (e) {
+      e.preventDefault();
+      window.location.href = "files/Sample.pdf";
+  });
+});
+// typining animation script
+var typed = new Typed(".herobanner__paragraph-span",{
+strings:["HTML Developer","JS Developer","Bootstrap5 Developing"],
+typeSpeed:100,
+backSpeed:100,
+loop:true
+});
