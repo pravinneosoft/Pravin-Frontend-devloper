dccdb9161e5708f59b97f9bdfba818b0ce123114
pravinneosoft <pravin.satpute@neosoftmail.com>
2022-03-24 10:52:55 +0530

    booket add

---
+++ booket/demo structure/html/assets/source/js/global.js
@@ -0,0 +1,38 @@
+jQuery = jQuery.noConflict();
+var KSCAPE = {
+	init: function() {
+		// init code goes here
+		//HTML5 Sementic elements
+		document.createElement("header");
+		document.createElement("nav");
+		document.createElement("main");
+		document.createElement("figure");
+		document.createElement("footer");
+
+		// Links with role button: Add click functionality when enter pressed
+		KSCAPE.buttonSpace();
+	},
+
+	/*
+	Function Name : KSCAPE.buttonSpace
+	Creates button behavior in links with button role
+	@version: 10-09-2019 updated
+	*/
+	buttonSpace: function() {		
+		
+	},
+};
+
+// when the page is ready, initialize everything
+jQuery(document).on('ready', function() {
+	KSCAPE.init();
+});
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:1,
+    dot:true,
+    margin:10,
+    nav:true,
+})
