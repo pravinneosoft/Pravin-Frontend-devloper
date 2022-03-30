e2943a659bb2e5243eea74672cc03e07065d6d44
pravinneosoft <pravin.satpute@neosoftmail.com>
2022-03-30 14:56:20 +0530

    update

--- booket/demo structure/html/assets/source/js/global.js
+++ booket/demo structure/html/assets/source/js/global.js
@@ -36,3 +36,13 @@ $('.owl-carousel').owlCarousel({
     margin:10,
     nav:true,
 })
+$(function () {
+	$('#datetimepicker5').datetimepicker({
+		defaultDate: "11/1/2013",
+		disabledDates: [
+			moment("12/25/2013"),
+			new Date(2013, 11 - 1, 21),
+			"11/22/2013 00:53"
+		]
+	});
+});
